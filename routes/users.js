d2c312904bea11093bf4addc982ba28b4d7bc692
Raheman Ansari <43606966+ARaheman@users.noreply.github.com>
2020-10-12 16:31:08 +0530

    added app name

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('sample-app');
+  res.send('simple-app');
 });
 
 module.exports = router;
