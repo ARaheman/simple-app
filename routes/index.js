5017084b3d820dbf168027779df4b7ba74df642e
Raheman Ansari <43606966+ARaheman@users.noreply.github.com>
2020-10-12 16:30:41 +0530

    added app name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'sample-app' });
+  res.render('index', { title: 'simple-app' });
   console.log('Hello ' + process.env.NAME)
 });
 
